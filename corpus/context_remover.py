795852de2315397711ad60c3d39e9ae8e196988e
morgan grinder <momo@schmorgasbord.hsd1.ca.comcast.net>
2020-10-28 15:50:17 -0700

    .ipynb

--- corpus/context_remover.py
+++ corpus/context_remover.py
@@ -2,7 +2,9 @@ import pandas as pd
 
 # with open("swbd.tab") as f:
     # contents = f.read()
-df = pd.read_csv("/results/swbd.tab", header=True)
+df = pd.read_csv("results/swbd.tab", sep='\t', lineterminator='\r')
+
+df_contexts = 
 
 
 
