342b30e3e22a7500c3b767e1baf5fc06c58e3a26
morgan grinder <momo@schmorgasbord.local>
2020-12-01 08:14:24 -0800

    lm models and graphs

--- analysis/rscripts/main_analysis.R
+++ analysis/rscripts/main_analysis.R
@@ -12,6 +12,7 @@ library(ggplot2)
 library(tidyr)
 library(dplyr)
 library(lme4)
+library(lmerTest)
 library(tidyverse)
 theme_set(theme_bw())
 
@@ -231,9 +232,305 @@ agr = test %>%
 
 # subject variablility
 ggplot(agr, aes(x=Question,y=mean_rating,fill=Question)) +
-  geom_bar(position="dodge",stat="identity") +
+  geom_bar(stat="identity") +
   geom_errorbar(aes(ymin=YMin,ymax=YMax), width=.25,position=position_dodge(0.9)) +
   facet_wrap(~paraphrase) +
-  theme(legend.position = "none") +
-  ggsave("../graphs/main_byitem.pdf")
+  theme(legend.position = "none")
+  # ggsave("../graphs/main_byitem.pdf")
 
+ggplot(agr, aes(x=mean_rating)) +
+  geom_histogram(stat="count") +
+  facet_wrap(~paraphrase)
+  # theme(legend.position = "none")
+
+
+the_high = agr %>%
+  filter(paraphrase %in% c("the") &  mean_rating > .5)
+
+the_high["Question"]  
+
+a_high = agr %>%
+  filter(paraphrase %in% c("a") &  mean_rating > .5)
+a_high["Question"]
+
+all_high = agr %>%
+  filter(paraphrase %in% c("all") &  mean_rating > .5)
+all_high["Question"]
+
+cbPalette <- c("#56B4E9", "#D55E00", "#009E73","#999999", "#E69F00","#009E73","#56B4E9", "#D55E00", "#009E73","#999999", "#E69F00","#009E73","#56B4E9", "#D55E00", "#009E73","#999999", "#E69F00","#009E73","#56B4E9", "#D55E00", "#009E73","#999999", "#E69F00","#009E73")
+########################################################################
+# Regression Models
+########################################################################
+# First break up data into each paraphrase,
+# Run logistic regression in each
+
+the = subset(test, test$paraphrase == "the")
+a = subset(test, test$paraphrase == "a")
+all = subset(test, test$paraphrase == "all")
+other = subset(test, test$paraphrase == "other")
+
+########################################################################
+# start with subjects only intercept
+
+# "The" Paraphrase
+m0 = lmer(rating ~ (1|workerid), data=the)
+# summary(m0)
+the$FittedOnlySubjectVar_the = fitted(m0)
+
+means = the %>%
+  group_by(tgrep_id) %>%
+  summarize(MeanPredicted = mean(FittedOnlySubjectVar_the),MeanEmpirical = mean(rating))
+View(means)
+ggplot(means, aes(x=MeanPredicted,y=MeanEmpirical)) +
+  geom_point() +
+  geom_smooth(method="lm") +
+  # xlim(0,1) +
+  # ylim(0,1) +
+  ylab("Empirical rating") +
+  xlab("Predicted rating")
+ggsave("../graphs/model_fit_0_the.pdf",width=5,height=4)
+cor(means$MeanEmpirical,means$MeanPredicted)
+
+
+# "A" Paraphrase
+m0 = lmer(rating ~ (1|workerid), data=a)
+# summary(m0)
+a$FittedOnlySubjectVar_a = fitted(m0)
+
+means = a %>%
+  group_by(tgrep_id) %>%
+  summarize(MeanPredicted = mean(FittedOnlySubjectVar_a),MeanEmpirical = mean(rating))
+ggplot(means, aes(x=MeanPredicted,y=MeanEmpirical)) +
+  geom_point() +
+  geom_smooth(method="lm") +
+  # xlim(0,1) +
+  # ylim(0,1) +
+  ylab("Empirical rating") +
+  xlab("Predicted rating")
+ggsave("../graphs/model_fit_0_a.pdf",width=5,height=4)
+cor(means$MeanEmpirical,means$MeanPredicted)
+
+
+# "All" Paraphrase
+m0 = lmer(rating ~ (1|workerid), data=all)
+all$FittedOnlySubjectVar_all = fitted(m0)
+
+means = all %>%
+  group_by(tgrep_id) %>%
+  summarize(MeanPredicted = mean(FittedOnlySubjectVar_all),MeanEmpirical = mean(rating))
+ggplot(means, aes(x=MeanPredicted,y=MeanEmpirical)) +
+  geom_point() +
+  geom_smooth(method="lm") +
+  # xlim(0,1) +
+  # ylim(0,1) +
+  ylab("Empirical rating") +
+  xlab("Predicted rating")
+ggsave("../graphs/model_fit_0_all.pdf",width=5,height=4)
+cor(means$MeanEmpirical,means$MeanPredicted)
+
+head(the)
+########################################################################
+# add fixed effects of interest with random subjects
+m.noitems = lmer(rating ~ ModalPresent + Wh + DeterminerSubjPresent + DeterminerNonSubjPresent  + (1|workerid), data=the) 
+summary(m.noitems)
+the$FittedNoCluster = fitted(m.noitems)
+
+means = the %>%
+  group_by(tgrep_id) %>%
+  summarize(MeanPredicted = mean(FittedNoCluster),MeanEmpirical = mean(rating))
+ggplot(means, aes(x=MeanPredicted,y=MeanEmpirical)) +
+  geom_point() +
+  geom_smooth(method="lm") +
+  # xlim(1,7) +
+  # ylim(1,7) +
+  ylab("Empirical rating") +
+  xlab("Predicted rating")
+ggsave("../graphs/model_fit_fixed_the.pdf",width=5,height=4)
+cor(means$MeanEmpirical,means$MeanPredicted)
+
+# don't work with current version of R
+r.squaredGLMM(m.noitems) 
+vif.mer(m.noitems) 
+
+
+m.noitems = lmer(rating ~ ModalPresent + Wh + DeterminerSubjPresent + DeterminerNonSubjPresent  + (1|workerid), data=a) 
+summary(m.noitems)
+a$FittedNoCluster = fitted(m.noitems)
+
+means = a %>%
+  group_by(tgrep_id) %>%
+  summarize(MeanPredicted = mean(FittedNoCluster),MeanEmpirical = mean(rating))
+View(means)
+ggplot(means, aes(x=MeanPredicted,y=MeanEmpirical)) +
+  geom_point() +
+  geom_smooth(method="lm") +
+  # xlim(1,7) +
+  # ylim(1,7) +
+  ylab("Empirical rating") +
+  xlab("Predicted rating")
+ggsave("../graphs/model_fit_fixed_a.pdf",width=5,height=4)
+cor(means$MeanEmpirical,means$MeanPredicted)
+
+# don't work with current version of R
+r.squaredGLMM(m.noitems) 
+vif.mer(m.noitems) 
+
+
+m.noitems = lmer(rating ~ ModalPresent + Wh + DeterminerSubjPresent + DeterminerNonSubjPresent  + (1|workerid), data=all) 
+summary(m.noitems)
+all$FittedNoCluster = fitted(m.noitems)
+
+means = all %>%
+  group_by(tgrep_id) %>%
+  summarize(MeanPredicted = mean(FittedNoCluster),MeanEmpirical = mean(rating))
+ggplot(means, aes(x=MeanPredicted,y=MeanEmpirical)) +
+  geom_point() +
+  geom_smooth(method="lm") +
+  # xlim(1,7) +
+  # ylim(1,7) +
+  ylab("Empirical rating") +
+  xlab("Predicted rating")
+ggsave("../graphs/model_fit_fixed_all.pdf",width=5,height=4)
+cor(means$MeanEmpirical,means$MeanPredicted)
+
+
+########################################################################
+# add fixed effects of interest no random effects
+
+# add fixed effects of interest
+m.noitemsnosubjects = lm(rating ~  ModalPresent + Wh + DeterminerSubjPresent + DeterminerNonSubjPresent, data=the)
+summary(m.noitemsnosubjects)
+the$FittedNoItemsNoSubject = fitted(m.noitemsnosubjects)
+
+means = the %>%
+  group_by(tgrep_id) %>%
+  summarize(MeanPredicted = mean(FittedNoItemsNoSubject),MeanEmpirical = mean(rating))
+ggplot(means, aes(x=MeanPredicted,y=MeanEmpirical)) +
+  geom_point() +
+  geom_smooth(method="lm") +
+  # xlim(1,7) +
+  # ylim(1,7) +
+  ylab("Empirical rating") +
+  xlab("Predicted rating")
+ggsave("../graphs/model_fit_fixed_norandom_the.pdf",width=5,height=4)
+cor(means$MeanEmpirical,means$MeanPredicted)
+
+m.noitemsnosubjects = lm(rating ~  ModalPresent + Wh + DeterminerSubjPresent + DeterminerNonSubjPresent, data=a)
+summary(m.noitemsnosubjects)
+a$FittedNoItemsNoSubject = fitted(m.noitemsnosubjects)
+
+means = a %>%
+  group_by(tgrep_id) %>%
+  summarize(MeanPredicted = mean(FittedNoItemsNoSubject),MeanEmpirical = mean(rating))
+ggplot(means, aes(x=MeanPredicted,y=MeanEmpirical)) +
+  geom_point() +
+  geom_smooth(method="lm") +
+  # xlim(1,7) +
+  # ylim(1,7) +
+  ylab("Empirical rating") +
+  xlab("Predicted rating")
+ggsave("../graphs/model_fit_fixed_norandom_a.pdf",width=5,height=4)
+cor(means$MeanEmpirical,means$MeanPredicted)
+
+m.noitemsnosubjects = lm(rating ~  ModalPresent + Wh + DeterminerSubjPresent + DeterminerNonSubjPresent, data=all)
+summary(m.noitemsnosubjects)
+all$FittedNoItemsNoSubject = fitted(m.noitemsnosubjects)
+
+means = all %>%
+  group_by(tgrep_id) %>%
+  summarize(MeanPredicted = mean(FittedNoItemsNoSubject),MeanEmpirical = mean(rating))
+ggplot(means, aes(x=MeanPredicted,y=MeanEmpirical)) +
+  geom_point() +
+  geom_smooth(method="lm") +
+  # xlim(1,7) +
+  # ylim(1,7) +
+  ylab("Empirical rating") +
+  xlab("Predicted rating")
+ggsave("../graphs/model_fit_fixed_norandom_all.pdf",width=5,height=4)
+cor(means$MeanEmpirical,means$MeanPredicted)
+
+
+########################################################################
+# add fixed effects of interest with random subjects and items intercepts
+m.noitems = lmer(rating ~ ModalPresent + Wh + DeterminerSubjPresent + DeterminerNonSubjPresent  + (1|workerid) + (1|tgrep_id), data=the) 
+summary(m.noitems)
+the$FittedNoClusterItem = fitted(m.noitems)
+
+means = the %>%
+  group_by(tgrep_id) %>%
+  summarize(MeanPredicted = mean(FittedNoClusterItem),MeanEmpirical = mean(rating))
+ggplot(means, aes(x=MeanPredicted,y=MeanEmpirical)) +
+  geom_point() +
+  geom_smooth(method="lm") +
+  # xlim(1,7) +
+  # ylim(1,7) +
+  ylab("Empirical rating") +
+  xlab("Predicted rating")
+ggsave("../graphs/model_fit_full_the.pdf",width=5,height=4)
+cor(means$MeanEmpirical,means$MeanPredicted)
+
+# plot of raw histogram
+ggplot(the, aes(x=rating)) +
+  geom_histogram()
+ggsave("../graphs/histogram_raw_the.pdf",width=4,height=3)
+
+# plot of means histogram
+ggplot(means, aes(x=MeanEmpirical)) +
+  geom_histogram()
+ggsave("../graphs/histogram_means_the.pdf",width=4,height=3)
+
+
+m.noitems = lmer(rating ~ ModalPresent + Wh + DeterminerSubjPresent + DeterminerNonSubjPresent  + (1|workerid) + (1|tgrep_id), data=a) 
+summary(m.noitems)
+a$FittedNoClusterItem = fitted(m.noitems)
+
+means = a %>%
+  group_by(tgrep_id) %>%
+  summarize(MeanPredicted = mean(FittedNoClusterItem),MeanEmpirical = mean(rating))
+ggplot(means, aes(x=MeanPredicted,y=MeanEmpirical)) +
+  geom_point() +
+  geom_smooth(method="lm") +
+  # xlim(1,7) +
+  # ylim(1,7) +
+  ylab("Empirical rating") +
+  xlab("Predicted rating")
+ggsave("../graphs/model_fit_full_a.pdf",width=5,height=4)
+cor(means$MeanEmpirical,means$MeanPredicted)
+
+# plot of raw histogram
+ggplot(a, aes(x=rating)) +
+  geom_histogram()
+ggsave("../graphs/histogram_raw_a.pdf",width=4,height=3)
+
+# plot of means histogram
+ggplot(means, aes(x=MeanEmpirical)) +
+  geom_histogram()
+ggsave("../graphs/histogram_means_a.pdf",width=4,height=3)
+
+
+m.noitems = lmer(rating ~ ModalPresent + Wh + DeterminerSubjPresent + DeterminerNonSubjPresent  + (1|workerid) + (1|tgrep_id), data=all) 
+summary(m.noitems)
+all$FittedNoClusterItem = fitted(m.noitems)
+
+means = all %>%
+  group_by(tgrep_id) %>%
+  summarize(MeanPredicted = mean(FittedNoClusterItem),MeanEmpirical = mean(rating))
+ggplot(means, aes(x=MeanPredicted,y=MeanEmpirical)) +
+  geom_point() +
+  geom_smooth(method="lm") +
+  # xlim(1,7) +
+  # ylim(1,7) +
+  ylab("Empirical rating") +
+  xlab("Predicted rating")
+ggsave("../graphs/model_fit_full_all.pdf",width=5,height=4)
+cor(means$MeanEmpirical,means$MeanPredicted)
+
+# plot of raw histogram
+ggplot(all, aes(x=rating)) +
+  geom_histogram()
+ggsave("../graphs/histogram_raw_all.pdf",width=4,height=3)
+
+# plot of means histogram
+ggplot(means, aes(x=MeanEmpirical)) +
+  geom_histogram()
+ggsave("../graphs/histogram_means_all.pdf",width=4,height=3)
