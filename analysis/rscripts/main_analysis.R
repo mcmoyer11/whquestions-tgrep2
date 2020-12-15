bb67a39f430455135a1ec4ca46c937dd166908db
thegricean <judithdegen@gmail.com>
2020-12-15 20:48:11 +0100

    changes to analysis

--- analysis/rscripts/main_analysis.R
+++ analysis/rscripts/main_analysis.R
@@ -5,8 +5,6 @@
 # ---
 
 ## Step 1: select stimuli for experiment
-setwd("/Users/momo/Dropbox/Stanford/whquestions-tgrep2/analysis/rscripts/")
-source("/Users/momo/Dropbox/Stanford/whquestions-tgrep2/analysis/helpers.R")
 library(ggplot2)
 library(tidyr)
 library(dplyr)
@@ -15,14 +13,38 @@ library(lmerTest)
 library(tidyverse)
 library(multcomp) # not available for this version of R
 theme_set(theme_bw())
+
+this.dir <- dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(this.dir)
+source("../helpers.R")
+
 cbPalette <- c("#56B4E9", "#D55E00", "#009E73","#999999", "#E69F00","#009E73","#56B4E9", "#D55E00", "#009E73","#999999", "#E69F00","#009E73","#56B4E9", "#D55E00", "#009E73","#999999", "#E69F00","#009E73","#56B4E9", "#D55E00", "#009E73","#999999", "#E69F00","#009E73")
 
 ########################################################################
 # Read the data into R.
-d = read.csv("/Users/momo/Dropbox/Stanford/whquestions-tgrep2/analysis/data/normed.csv")
+d = read.csv("../data/normed.csv")
+
+contrasts(d$ModalPresent)
+contrasts(d$Wh) = cbind("how.vs.where"=c(1,0,0),"who.vs.where"=c(0,0,1))
+contrasts(d$paraphrase) = cbind("a.vs.every"=c(1,0,0),"the.vs.every"=c(0,0,1))
 
 # Full Model with random slopes
-m = lmerTest::lmer(rating ~ ModalPresent*Wh*paraphrase + (1+ModalPresent|workerid) + (1+Wh|workerid) + (1+paraphrase|workerid) + (1|tgrep_id), data=d,REML=FALSE) 
+m = lmerTest::lmer(rating ~ ModalPresent*Wh*paraphrase + (1+paraphrase|workerid) + (1+paraphrase|tgrep_id), data=d,REML=FALSE) 
+# message?: boundary (singular) fit: see ?isSingular
+summary(m)
+
+m.twowayinteraction = lmer(rating ~ ModalPresent+Wh+paraphrase + ModalPresent:paraphrase + Wh:paraphrase + (1+paraphrase|workerid) + (1+paraphrase|tgrep_id), data=d,REML=FALSE) 
+summary(m.twowayinteraction)
+
+
+m.full = lmer(rating ~ ModalPresent+Wh+paraphrase + (1+paraphrase|workerid) + (1+paraphrase|tgrep_id), data=d,REML=FALSE) 
+summary(m.full)
+
+m.noModal = lmer(rating ~ Wh+paraphrase + (1+paraphrase|workerid) + (1+paraphrase|tgrep_id), data=d,REML=FALSE) 
+
+anova(m.noModal,m.full)
+
+m = lmerTest::lmer(rating ~ ModalPresent + Wh + paraphrase + (1+paraphrase|workerid) + (1+paraphrase|tgrep_id), data=d,REML=FALSE) 
 # message?: boundary (singular) fit: see ?isSingular
 summary(m)
 
